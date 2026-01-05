98993e4a78f20783ce9b7a3df1733f93ee65d22b
noob-dev <tejasvibajaj2@gmail.com>
2026-01-05 15:58:42 +0530

    Delete config directory

--- config/core/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-'''
-Not in use for this project as of now ;-;
-'''
--- config/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import start_download, task_status, download_file,home
-
-urlpatterns = [
-    path("", home),
-    path("download/", start_download),
-    path("status/<str:task_id>/", task_status),
-    path("file/<str:task_id>/", download_file),
-]
